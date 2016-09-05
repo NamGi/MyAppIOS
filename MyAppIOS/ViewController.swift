d98aed9814595be9a4f35a684906801e89d55e20
김남기 <kimnamgi@gimnamgiui-macbook-pro.local>
2016-09-05 11:54:29 +0900

    Initial Commit

---
+++ MyAppIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAppIOS
+//
+//  Created by 김남기 on 2016. 9. 5..
+//  Copyright © 2016년 NamGi Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
